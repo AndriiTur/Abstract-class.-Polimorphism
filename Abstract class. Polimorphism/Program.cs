4d7534e94d70b64e5b04bba9e95aca5a776c7691
Andrii Tur <andriiturkevych@gmail.com>
2018-09-13 00:26:51 +0300

    Добавьте файлы проекта.

---
+++ Abstract class. Polimorphism/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_class.Polimorphism
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
